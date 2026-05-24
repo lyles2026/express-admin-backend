1d61184e726fd5e26906272359bd3262d7ebae16
路由 <m19373557587@163.com>
2026-05-24 23:07:37 +0800

    设置兼容环境变量

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mydb')
+        const mongoUri = process.env.MONGO_URI || process.env.MONGO_URL || 'mongodb://localhost:27017/mydb'
+        await mongoose.connect(mongoUri)
         console.log('MongoDB 连接成功!');
     } catch (error) {
         console.error('Database connection error:', error);
